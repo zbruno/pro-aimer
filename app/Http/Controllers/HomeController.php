d932f4a59ddca416b40d5607269d48de57c87dd7
Zachary Bruno <zachcbruno@gmail.com>
2016-05-30 22:16:36 -0700

    Setup initial controller and view

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Show the application dashboard.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+}
